3ea3b25a0341ebdb522fa6bed9541f937b10a8e7
Luis Miguel Rodriguez Herrera <l.miguel0529@hotmail.com>
2020-09-04 23:22:17 -0500

    add all proyect Server config Spring Cloud

---
+++ src/test/java/com/formacionbdi/springboot/app/configserver/SpringbootServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
